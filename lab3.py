96da09f23b47baca956a86c2868aee57a8ec9a61
obarquero <obarquero@gmail.com>
2014-12-04 13:41:41 +0100

    Changes looking for the work done by Carlos
    Something is wrong, my guess is that we need to scale the data before
    apply pca.

--- lab3.py
+++ lab3.py
@@ -10,10 +10,11 @@ import os as os
 #import csv as csv
 import pandas as pd
 from sklearn.decomposition import PCA
+import numpy as np
 #import sklearn.preprocessing as prepro
 #import statsmodels.formula.api as sm
 #import statsmodels.stats.outliers_influence as vif
-#import matplotlib.pylab as plt
+import matplotlib.pylab as plt
 from sklearn.cross_validation import train_test_split
 #from sklearn.grid_search import GridSearchCV
 #import sklearn.metrics as metrics
@@ -27,7 +28,7 @@ from sklearn import datasets, linear_model
 from sklearn import cross_validation
 
 #plt.ioff()
-path = '/home/carlos/TFG-carlos-biedma-tapia'
+path = '/Users/obarquero/Qsync/TFGs/TFG_Carlos_Biedma/python_code/TFG-carlos-biedma-tapia/'
 os.chdir(path)
 fname = 'hitters_data2.csv'
 #wild_boar_data = pd.read_csv(fname,delimiter = ";") # this reads the data using panda
@@ -85,10 +86,34 @@ for m in range(n_features):
     scores_std.append(np.std(this_scores)) #desviación estándar
     #print scores_std
 
-plot(scores)
+plt.plot(scores)
+plt.show()
 
+#Algo no estamos haciendo bien
+#
+#1) 
+#Acuerdate de poner nombres a los ejes
+#2)
+#No está claro que los resultados sean como los que se obtienen en el lab3. 
+#si te fijas en su caso dice que M=7 components es lo que se elije utilizando CV
+#en la gráfica que tu presentas siempre se desciende. De todas formas no sé qué 
+#es lo que se representa en el eje Y. Date cuenta de que el valor es negativo,
+# tendrías que verificar que score está utilizando clf y modificarlo para que 
+#sea RMSE, igual que lo que están utilizando en el libro para que podamos comparar.
+#Aquí te ayudo yo: si miras la ayuda del sklearn verás que el score que utiliza 
+#linear regression por defecto es 
+#score(X, y[, sample_weight]) 	Returns the coefficient of determination R^2 of the prediction.
 
-#Now plot all the scores and select which is the number of components
+#3)También sería conveniente primero que verificasemos que los valores en Training
+#son equivalentes a los que obtienen en el lab3. Si miras los resultados de % variance
+#explained verás que usando una componente la varianza explicada es 38,31, sin embargo lo que
+# te da a tí es
+pca_ex.pca_ex.explained_variance_ratio_[0]
+#99,9
+#Algo está mal
+#4) Mi hipótesis es que hay primero que transformar los datos: centrarlos y normalizarlos
+#si le echas un vistazo al script que te adjunté yo verás cómo se realizar con un objeto
+#de sklearn que se llama preprocessing.
 
 
 
