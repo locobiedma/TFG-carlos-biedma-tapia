35ba6b1ac0d2a3de637d6ebff56ca6843a4a1e32
carlos biedma tapia <carlos.biedma.tapia@gmail.com>
2015-01-27 23:58:34 +0100

    nuevo lab

--- 6.7.2Nuevo.py
+++ 6.7.2Nuevo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 20 19:18:38 2014
+Created on Tue Jan 27 23:01:40 2015
 
 @author: carlos
 """
@@ -29,6 +29,7 @@ from sklearn import cross_validation
 import sklearn.preprocessing as prepro
 from sklearn import metrics
 from matplotlib.pyplot import *
+from sklearn.cross_decomposition import PLSRegression
 
 #plt.ioff()
 path = '/home/carlos/TFG-carlos-biedma-tapia'
@@ -60,38 +61,25 @@ scaler.fit(wb_data)
 wb_data = scaler.transform(wb_data)
 #%%
 
-
-#Scatter plots for each variable
-#for i in wild_boar_data.columns:
- #   pd.tools.plotting.scatter_plot(wild_boar_data,i,'AtBat')
-#%%
-
 X = np.concatenate((wb_data[:,0:-2],wb_data[:,0:-1:]),axis = 1) #¿Con esto te quitas la última columna?
 #print(X)
 X2 = wb_data[:,:]
-print X2
+#print X2
 #%%
 #print "------------------------------------"
 Y = wb_data[:,-2]
-print(Y)
+#print(Y)
 #%%
+pls_wild_b = PLSRegression(n_components = 20) #No sé que número poner.
+pls_wild_b.fit(X2,Y)
+Z = pls_wild_b.transform(X2)
 
-#1) Create a PCA object
-pca_ex = PCA()
-
-#2) Let's compute real pca
-pca_ex.fit(X2) #Este creo que no lo uso para nada; Fit the model with X.
-
-Z = pca_ex.transform(X2) #Apply the dimensionality reduction on X; 
-#X is projected on the first principal components previous extracted from a training set.
-#%%
-#print Z
 
 scores = list() 
 scores_std = list()
 
 n_features = np.shape(Z)[1]
-print(n_features) 
+print(n_features) #¿Aquí no debería ser un numero menor de 20?
 print np.shape(Z)
 
 
@@ -123,45 +111,14 @@ for m in range(n_features):
 plt.plot(scores)
 xlabel('Componentes')
 ylabel("$MSE$")
-title("lab3")
+title("6.7.2")
 plt.show()
 
 print "% Variance Explained (cumulative)"
 print np.cumsum(pca_ex.explained_variance_ratio_)
 
 
-#Algo no estamos haciendo bien
-#
-#1) 
-#Acuerdate de poner nombres a los ejes
-#2)
-#No está claro que los resultados sean como los que se obtienen en el lab3. 
-#si te fijas en su caso dice que M=7 components es lo que se elije utilizando CV
-#en la gráfica que tu presentas siempre se desciende. De todas formas no sé qué 
-#es lo que se representa en el eje Y. Date cuenta de que el valor es negativo,
-# tendrías que verificar que score está utilizando clf y modificarlo para que 
-#sea RMSE, igual que lo que están utilizando en el libro para que podamos comparar.
-#Aquí te ayudo yo: si miras la ayuda del sklearn verás que el score que utiliza 
-#linear regression por defecto es 
-#score(X, y[, sample_weight]) 	Returns the coefficient of determination R^2 of the prediction.
-
-#3)También sería conveniente primero que verificasemos que los valores en Training
-#son equivalentes a los que obtienen en el lab3. Si miras los resultados de % variance
-#explained verás que usando una componente la varianza explicada es 38,31, sin embargo lo que
-# te da a tí es
 pca_ex.explained_variance_ratio_[0]
-#99,9
-#Algo está mal
-#4) Mi hipótesis es que hay primero que transformar los datos: centrarlos y normalizarlos
-#si le echas un vistazo al script que te adjunté yo verás cómo se realizar con un objeto
-#de sklearn que se llama preprocessing.
-
-
-
-#There should make pca in training data
-
-
-
 
 
 
